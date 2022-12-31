07f0b3307cbff1e2a0d5a4212ef30b0ee9cf8db1
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2022-12-31 20:07:13 +0600

    Update

--- dist/script1.js
+++ dist/script1.js
@@ -3,3 +3,5 @@
 export const a = 1;
 export const b = 1;
 export const c = 3;
+
+export default { a, b, c};
--- dist/script2.js
+++ dist/script2.js
@@ -1,7 +1,8 @@
 'use strict';
 
-import { a, b, c, c as copyOfC } from './script1.js';
+import defaultImport, { a, b, c, c as copyOfC } from './script1.js';
 
+console.log('defaultImport', defaultImport);
 console.log('a', a);
 console.log('b', b);
 console.log('c', c);
